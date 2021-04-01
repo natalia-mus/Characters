65e6a445ab68bd76c43a85d4419f9188c6849d8a
natalia-mus <natalia.muskala1ab1@gmail.com>
2021-04-01 22:33:49 +0200

    CharacterItem class optimalization

--- app/src/main/java/com/example/characters_api_mvvm/model/CharacterItem.kt
+++ app/src/main/java/com/example/characters_api_mvvm/model/CharacterItem.kt
@@ -7,26 +7,12 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class CharacterItem(
-    /*@SerializedName("appearance")
-    val appearance: List<Any>,*/
-    @SerializedName("better_call_saul_appearance")
-    val betterCallSaulAppearance: List<Int>,
-    @SerializedName("birthday")
     val birthday: String,
-    @SerializedName("category")
-    val category: String,
     @SerializedName("char_id")
     val id: Int,
     @SerializedName("img")
     val image: String,
-    @SerializedName("name")
     val name: String,
-    @SerializedName("nickname")
     val nickname: String,
-    @SerializedName("occupation")
-    val occupation: List<String>,
-    @SerializedName("portrayed")
-    val portrayed: String,
-    @SerializedName("status")
     val status: String
 ) : Parcelable
